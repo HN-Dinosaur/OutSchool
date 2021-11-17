ea2cf1df537f4fa5dda3270b6bdd2c9fe5c29ec8
HN-Dinosaur <464050079@qq.com>
2021-11-17 18:51:11 +0800

    实现基本界面

--- OutSchool/FirstTableViewCell.swift
+++ OutSchool/FirstTableViewCell.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class FirstTableViewCell: UITableViewCell {
 
+    let myImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        imageView.backgroundColor = .red
+        return imageView
+    }()
     override func awakeFromNib() {
         super.awakeFromNib()
+        contentView.addSubview(myImageView)
+        myImageView.snp.makeConstraints { make in
+            make.top.bottom.right.equalToSuperview().offset(5)
+        }
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
--- OutSchool/InfoViewController.swift
+++ OutSchool/InfoViewController.swift
@@ -6,66 +6,87 @@
 //
 
 import UIKit
-
+import SnapKit
 class InfoViewController: UIViewController {
-    let label: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .secondaryLabel
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textColor = .label
-        label.textAlignment = .center
-        
-        return label
+    var isExpanding: [Bool] = [false, false, false]
+    var strArray: [String] = ["基本信息","申请信息","审核信息"]
+    var viewArray: [UIView] = [
+            Bundle.loadNibView(name: "View1", class: UIView.self),
+            Bundle.loadNibView(name: "View2", class: UIView.self),
+            Bundle.loadNibView(name: "View3", class: UIView.self)
+    ]
+    let infoTabelView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        return tableView
     }()
-
+    let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var bottomLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            label.topAnchor.constraint(equalTo: view.topAnchor),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            label.heightAnchor.constraint(equalToConstant: 44),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
-            
-        ])
-        view.addSubview(label)
-        view.addSubview(tableView)
-        // Do any additional setup after loading the view.
+        infoTabelView.delegate = self
+        infoTabelView.dataSource = self
+        mainView.addSubview(infoTabelView)
+        infoTabelView.snp.makeConstraints { make in
+            make.left.right.equalTo(view)
+            make.top.equalTo(topView.snp.bottom)
+            make.bottom.equalTo(bottomLabel.snp.top)
+        }
+        infoTabelView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 extension InfoViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let cell = tableView.cellForRow(at: indexPath)
+//        tableView.footerView(forSection: indexPath.section)?.bounds.size.height = 0
+        isExpanding[indexPath.section] = !isExpanding[indexPath.section]
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 extension InfoViewController: UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        4
-    }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        0.0001
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        UIView()
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return viewArray[section].bounds.height
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return viewArray[section]
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        3
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        cell.textLabel?.text = "123"
+        cell.textLabel?.text = strArray[indexPath.section]
+//        cell.accessoryView = UIImageView(image: UIImage(named: "chevronUp"))
+//        ce
+//        cell.accessoryType = .detailDisclosureButton
+        cell.accessoryType = .disclosureIndicator
+        
         return cell
     }
-    
-    
 }
+extension Bundle{
+    static func loadNibView<T>(name: String, class: T.Type) -> T{
+        if let view = main.loadNibNamed(name, owner: nil, options: nil)?.first as? T{
+            return view
+        }
+        fatalError("获取BundleView出现问题")
+    }
+}
+
